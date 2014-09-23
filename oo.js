804eb370f3ddff4ac3ed8e66a2962e498d153ebc
DK <dukai86@gmail.com>
2014-09-24 00:46:12 +0800

    update

---
+++ oo.js
@@ -0,0 +1,42 @@
+(function(win, undefined){
+
+var extend = function(subClass, baseClass){
+	var parent = subClass.parent = {
+		/**
+		 * parent construct
+		 * @param obj currentObject
+		 * @param args
+		 */
+		'__construct': function(obj, args){
+			baseClass.apply(obj, args);
+		}
+	};
+
+	for(var method in baseClass.prototype){
+		parent[method] = baseClass.prototype[method];
+		if(! (method in subClass.prototype)){
+			subClass.prototype[method] = baseClass.prototype[method];
+		}
+	}
+};
+
+window.extend = extend;
+
+var mix = function(base, child, deep){
+    var o = new Object();
+    for(var key in base){
+        o[key] = base[key];
+    }
+    for(var key in child){
+		if(deep && isPlainObject(o[key])){
+			o[key] = mix(o[key], child[key]);
+		}else{
+			o[key] = child[key];
+		}
+    }
+    return o;
+};
+
+window.mix = mix;
+})(window);
+
